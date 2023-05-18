b0cb2ea388ebb8148114f1f5f9304beda6767fcb
범수 <delt96@naver.com>
2023-05-18 20:24:26 +0900

    2023-05-18 0단계2 1단계2

---
+++ src/Coding_test/test_2023_05_18_1.java
@@ -0,0 +1,13 @@
+package Coding_test;
+/*
+ * 다음과 같이 출력하도록 코드를 작성해 주세요.
+
+	출력 예시
+	
+	!@#$%^&*(\'"<>?:;
+ */
+public class test_2023_05_18_1 {
+    public static void main(String[] args) {
+        System.out.print("!@#$%^&*(\\\'\"<>?:;");
+    }
+}
