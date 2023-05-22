e347f7a8aaa526422d2760b8540ff955aec9a252
범수 <delt96@naver.com>
2023-05-22 20:40:57 +0900

    2023-05-22 0단계 2개 1단계 2개

---
+++ src/Coding_test/test_2023_05_22_2.java
@@ -0,0 +1,34 @@
+package Coding_test;
+
+import java.util.Scanner;
+
+/*
+	문자열 str이 주어집니다.
+	문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
+	
+	제한사항
+	1 ≤ str의 길이 ≤ 10
+	
+	입출력 예
+	입력 #1
+	
+	abcde
+	출력 #1
+	
+	a
+	b
+	c
+	d
+	e
+ */
+public class test_2023_05_22_2 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char[] b = a.toCharArray();
+        for(int i =0 ;i< b.length; i++ ) {
+        	System.out.println(b[i]);
+        }
+        
+    }
+}
